7f91c552961db735bd2fbe237b13e5ab9ff28e9d
PutraZC <putrazc@proton.me>
2024-07-11 16:16:22 +0700

    add missing command/message/error handler
    bot should working normal now

--- main.py
+++ main.py
@@ -50,5 +50,11 @@ if __name__ == "__main__":
     print("Starting bot...")
     app = Application.builder().token(TOKEN).build()
 
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    app.add_error_handler(error)
     print('Polling bot...')
     app.run_polling(poll_interval=5)
