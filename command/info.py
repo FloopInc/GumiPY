66c26d8688acbbcdc6d8bf6fde03d4efdc209342
Narhwal <spamazh1@gmail.com>
2024-09-25 19:10:55 +0800

    Avoid non owner/mod search another users!

--- command/info.py
+++ command/info.py
@@ -1,13 +1,14 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from handler.profile import inspectProfile,loadUserProfile
-from handler.register import isBanned,isRegistered,getTextMap,loadConfig
+from handler.register import isBanned,isRegistered,getTextMap,loadConfig,isMod
 import json
 with open('data/items.json','r')as file:items_data=json.load(file)
 async def info_command(update,context):
 	K='name';A=update;C=A.message.from_user.id;L=A.message.text.strip();M=loadConfig();G=L.split(' ')
 	if isBanned(C):await A.message.reply_text(getTextMap('isBanned'));return
 	if not isRegistered(C):await A.message.reply_text(getTextMap('notRegistered'));return
+	if C!=M and not isMod(C)and len(G)>1:await A.message.reply_text(getTextMap('onlyOwner1'));return
 	if len(G)>1:D=G[1]
 	else:D=C
 	E,H=inspectProfile(D);N=loadUserProfile(D)
